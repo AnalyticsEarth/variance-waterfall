819f0fe6fe9f2a6a099e040a65b0481ee6494e84
niekvanstaveren <niek.vanstaveren@qlik.com>
2020-11-27 16:41:58 +0100

    fix: await rendering for download

--- src/paint.js
+++ src/paint.js
@@ -1,14 +1,14 @@
-import ThemeManager from './theme';
+import ThemeManager from "./theme";
 
-export default function($element, layout) {
+export default function ($element, layout) {
   let app = this.$scope.qlik.currApp(this);
   let isEditMode = this.$scope.options.interactionState === 2;
   this.$scope.updatedData(layout, isEditMode, true);
-  app.theme.getApplied().then(qTheme => {
+  return app.theme.getApplied().then(async (qTheme) => {
     this.$scope.theme = qTheme;
     ThemeManager.setAppTheme(this.$scope.theme);
 
-    this.$scope.updatedData(layout, isEditMode, true);
+    await this.$scope.updatedData(layout, isEditMode, true);
     return this.$scope.qlik.Promise.resolve();
   });
 }
