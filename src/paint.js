b51bf4f6851a9a5330640dc894abb4ee845232ad
niekvanstaveren <niek.vanstaveren@qlik.com>
2021-10-26 13:47:57 +0200

    fix: paint when layout has changed

--- src/paint.js
+++ src/paint.js
@@ -4,6 +4,8 @@ export default function ($element, layout) {
   let app = this.$scope.qlik.currApp(this);
   let isEditMode = this.$scope.options.interactionState === 2;
   this.$scope.updatedData(layout, isEditMode, true);
+  this._pureLayout =
+    this.backendApi.model.pureLayout || this.backendApi.model.layout;
   return app.theme.getApplied().then(async (qTheme) => {
     this.$scope.theme = qTheme;
     ThemeManager.setAppTheme(this.$scope.theme);
--- src/resize.js
+++ src/resize.js
@@ -1,4 +1,7 @@
-export default function($element, layout) {
+export default function ($element, layout) {
+  if (this._pureLayout !== this.backendApi.model.pureLayout) {
+    return this.paint($element, layout);
+  }
   let isEditMode = this.$scope.options.interactionState === 2;
   this.$scope.updatedData(layout, isEditMode, false);
   return this.$scope.qlik.Promise.resolve();
